fcc8986caae5cfd8035115b1428ed4da15d753a9
Vladimir Y <dev.vladimir.y@gmail.com>
2017-12-30 03:59:03 +0100

    adjust e2e tests struture

--- src/e2e/index.spec.ts
+++ src/e2e/index.spec.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import {promisify} from "util";
 import test from "ava";
 
-import {actions, catchError, ENV, initApp, TestContext} from "./util";
+import {actions, catchError, CONF, ENV, initApp, TestContext} from "./util";
 
 test.beforeEach(async (t: TestContext) => {
     await initApp(t, {initial: true});
@@ -13,21 +13,39 @@ test.afterEach.always(actions.destroyApp.bind(actions));
 
 test("login, add account, logout, auto login", async (t: TestContext) => {
     try {
-        await actions.login(t, {setup: true, savePassword: false});
-        await actions.addAccount(t); // TODO make sure there is 1 account added
-        await actions.logout(t);
-        await actions.login(t, {setup: false, savePassword: true});
-        await actions.destroyApp(t);
+        await (async () => {
+            const client = t.context.app.client;
+
+            await client.pause(CONF.timeouts.transition);
+
+            await actions.login(t, {setup: true, savePassword: false});
+            await client.pause(CONF.timeouts.transition);
+
+            await actions.addAccount(t); // TODO make sure there is 1 account added
+            await client.pause(CONF.timeouts.transition);
+
+            await actions.logout(t);
+            await client.pause(CONF.timeouts.transition);
+
+            await actions.login(t, {setup: false, savePassword: true});
+            await client.pause(CONF.timeouts.transition);
+
+            t.is(
+                (await client.getUrl()).split("#").pop(), "/(accounts-outlet:accounts)",
+                `login: "accounts" page url`,
+            );
+            // TODO make sure there is 1 account added
+
+            await actions.destroyApp(t);
+        })();
 
         await initApp(t, {initial: false, outputDirPath: t.context.userDataDirPath});
-
-        await t.context.app.client.pause(1500);
+        await t.context.app.client.pause(CONF.timeouts.transition + CONF.timeouts.encryption);
 
         t.is(
             (await t.context.app.client.getUrl()).split("#").pop(), "/(accounts-outlet:accounts)",
-            `"accounts" page url`,
+            `root: "accounts" page url`,
         );
-        // TODO make sure added account is still there
 
         // making sure log file has not been created (no errors happened)
         t.false(fs.existsSync(t.context.logFilePath), `"${t.context.logFilePath}" file should not exist`);

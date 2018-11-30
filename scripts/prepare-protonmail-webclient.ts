8ec10e72b6c5fd47afe8035ac79087c588dc2bde
Vladimir Y <dev.vladimir.y@gmail.com>
2018-12-01 02:22:53 +0300

    tweak protonmail webclient clone/build script

--- scripts/prepare-protonmail-webclient.ts
+++ scripts/prepare-protonmail-webclient.ts
@@ -9,9 +9,11 @@ import {Arguments} from "src/shared/types";
 import {PROVIDER_REPO} from "src/shared/constants";
 
 // tslint:disable-next-line:no-console
-const log = console.log;
+const consoleLog = console.log;
+// tslint:disable-next-line:no-console
+const consoleError = console.error;
 const distDir = process.argv[2];
-const repoDir = path.resolve(process.cwd(), "./output/git/protonmail/webclient");
+const repoDir = path.resolve(process.cwd(), `./output/git/protonmail/webclient/${PROVIDER_REPO.protonmail.commit}`);
 const repoDistDir = path.resolve(repoDir, "./dist");
 const foldersAsDomains = [
     "mail.protonmail.com",
@@ -20,32 +22,38 @@ const foldersAsDomains = [
 (async () => {
     for (const folder of foldersAsDomains) {
         const resolvedDistDir = path.resolve(distDir, folder);
-        log(chalk.blue(`Preparing "${resolvedDistDir}" build`));
+        consoleLog(chalk.magenta(`Preparing built-in WebClient build:`), resolvedDistDir);
+
+        if (await fsExtra.pathExists(resolvedDistDir)) {
+            consoleLog(chalk.yellow(`Skipping as directory already exists:`), resolvedDistDir);
+            continue;
+        }
 
         if (await fsExtra.pathExists(repoDir)) {
-            log(chalk.yellow(`Skipping cloning`));
+            consoleLog(chalk.yellow(`Skipping cloning`));
         } else {
             await fsExtra.ensureDir(repoDir);
             await clone(repoDir);
         }
 
         if (await fsExtra.pathExists(path.resolve(repoDir, "node_modules"))) {
-            log(chalk.yellow(`Skipping dependencies installing`));
+            consoleLog(chalk.yellow(`Skipping dependencies installing`));
         } else {
             await installDependencies(repoDir);
         }
 
         if (await fsExtra.pathExists(repoDistDir)) {
-            log(chalk.yellow(`Skipping building`));
+            consoleLog(chalk.yellow(`Skipping building`));
         } else {
             await build(repoDir);
         }
 
+        consoleLog(chalk.magenta(`Copying:`), `${repoDistDir}" to "${resolvedDistDir}`);
         await fsExtra.copy(repoDistDir, resolvedDistDir);
     }
 })().catch((error) => {
-    log(chalk.red(error));
-    throw error;
+    consoleError("Uncaught exception", error);
+    process.exit(1);
 });
 
 async function build(dir: string) {
@@ -78,12 +86,12 @@ async function _execGit([commands, pathArg, options]: Arguments<typeof GitProces
         pathArg,
         {
             processCallback: ({stderr}) => {
-                byline(stderr).on("data", (chunk) => log(chunk.toString()));
+                byline(stderr).on("data", (chunk) => consoleLog(formatChunk(chunk)));
             },
             ...options,
         },
     ];
-    log(chalk.blue(`Executing "${JSON.stringify(args)}" Git command`));
+    consoleLog(chalk.magenta(`Executing Git command:`), JSON.stringify(args));
     const result = await GitProcess.exec(...args);
 
     if (result.exitCode) {
@@ -92,13 +100,30 @@ async function _execGit([commands, pathArg, options]: Arguments<typeof GitProces
 }
 
 async function _exec(args: Arguments<typeof spawnAsync>) {
-    log(chalk.blue(`Executing "${JSON.stringify(args)}" Shell command`));
+    consoleLog(chalk.magenta(`Executing Shell command: `), JSON.stringify(args));
 
-    const command = spawnAsync(...args);
-    byline(command.child.stdout).on("data", (chunk) => log(chunk.toString()));
-    const {status: exitCode} = await command;
+    const taskPromise = spawnAsync(...args);
 
-    if (exitCode) {
-        throw new Error(`Failed to execute "${JSON.stringify(args)}" command`);
+    byline(taskPromise.child.stdout).on("data", (chunk) => consoleLog(formatChunk(chunk)));
+    byline(taskPromise.child.stderr).on("data", (chunk) => consoleError(formatChunk(chunk)));
+    taskPromise.child.on("uncaughtException", (error) => {
+        consoleError(`Failed Shell command execution (uncaught exception): ${JSON.stringify(args)}`, error);
+        process.exit(1);
+    });
+
+    try {
+        const {status: exitCode} = await taskPromise;
+
+        if (exitCode) {
+            consoleError(`Failed Shell command execution (${exitCode} exit code): ${JSON.stringify(args)}`);
+            process.exit(exitCode);
+        }
+    } catch (error) {
+        consoleError(`Failed Shell command execution: ${JSON.stringify(args)}`, error.stack);
+        process.exit(1);
     }
+}
+
+function formatChunk(chunk: any): string {
+    return Buffer.from(chunk, "UTF-8").toString();
 }

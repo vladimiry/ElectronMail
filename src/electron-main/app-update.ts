3c96bea74603c45b3a2eb6716913ac75a1767200
Vladimir Y <dev.vladimir.y@gmail.com>
2018-12-09 14:07:23 +0300

    log auto update call errors with "warn" log level

--- src/electron-main/app-update.ts
+++ src/electron-main/app-update.ts
@@ -1,12 +1,8 @@
 import logger from "electron-log";
 import {autoUpdater} from "@vladimiry/electron-updater";
 
+// TODO switch to a regular "electron-updater" as soon as this PR get merged https://github.com/electron-userland/electron-builder/pull/3531
 export function initAutoUpdate() {
-    const catchError = (error: Error) => {
-        // TODO ignore "no internet connection" error only, and re-throw the other
-        logger.error(error);
-    };
-
     autoUpdater.logger = logger;
 
     try {
@@ -14,4 +10,9 @@ export function initAutoUpdate() {
     } catch (error) {
         catchError(error);
     }
+}
+
+function catchError(error: Error) {
+    // TODO ignore "no internet connection"-like errors only, and re-throw the others
+    logger.warn(error);
 }

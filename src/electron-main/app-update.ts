de8844ed7f1ed5bb28e78cda9137c2216d94d149
Vladimir Y <dev.vladimir.y@gmail.com>
2018-12-19 16:59:10 +0300

    final v2.0.0 preparation

--- src/electron-main/app-update.ts
+++ src/electron-main/app-update.ts
@@ -2,7 +2,6 @@ import logger from "electron-log";
 import {autoUpdater} from "electron-updater";
 
 export function initAutoUpdate() {
-    autoUpdater.channel = "beta";
     autoUpdater.allowDowngrade = false;
     autoUpdater.logger = logger;
 

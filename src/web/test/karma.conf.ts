a31d8bda72e7177eb9a97279e6c5e56dbcfa89d6
Vladimir Y <dev.vladimir.y@gmail.com>
2019-03-02 14:02:05 +0300

    make karma use "Chromium" as default browser

--- src/web/test/karma.conf.ts
+++ src/web/test/karma.conf.ts
@@ -8,52 +8,61 @@ const basePath = process.cwd();
 const filesBootstrap = "src/web/test/bootstrap.ts";
 const filesPattern = "src/web/src/**/*.spec.ts";
 
+const customLaunchers = {
+    linux: {
+        base: "Chrome",
+        flags: ["--no-sandbox"],
+    },
+    osx: {
+        base: "Firefox",
+        flags: ["-headless"],
+    },
+};
+
+const ENVS = {
+    CI: process.env.CI,
+    TRAVIS_OS_NAME: String(process.env.TRAVIS_OS_NAME),
+};
+
+const configuration = {
+    basePath,
+    files: [
+        filesBootstrap,
+        {pattern: filesPattern, watched: false},
+    ],
+    frameworks: ["jasmine"],
+    preprocessors: {
+        [filesBootstrap]: ["webpack"],
+        [filesPattern]: ["webpack"],
+    },
+    reporters: ["progress", "mocha"],
+    webpack: produce(webpackConfig, (draft) => {
+        // TODO get rid of "karma-webpack" hanging workaround
+        // https://github.com/webpack-contrib/karma-webpack/issues/322#issuecomment-417862717
+        (draft.output = draft.output || {}).filename = "[name]";
+    }),
+    webpackServer: {noInfo: true},
+    port: 9876,
+    colors: true,
+    logLevel: "INFO",
+    mime: {
+        "text/x-typescript": ["ts"],
+    },
+    browsers: [
+        "Chromium",
+    ],
+    customLaunchers,
+};
+
+if (ENVS.CI) {
+    configuration.browsers = ["Chrome"];
+}
+
+if (ENVS.TRAVIS_OS_NAME in customLaunchers) {
+    configuration.browsers = [ENVS.TRAVIS_OS_NAME];
+}
+
 export default (config: Config) => {
-    const TRAVIS_OS_NAME = String(process.env.TRAVIS_OS_NAME);
-    const customLaunchers = {
-        linux: {
-            base: "Chrome",
-            flags: ["--no-sandbox"],
-        },
-        osx: {
-            base: "Firefox",
-            flags: ["-headless"],
-        },
-    };
-    const configuration = {
-        basePath,
-        files: [
-            filesBootstrap,
-            {pattern: filesPattern, watched: false},
-        ],
-        frameworks: ["jasmine"],
-        preprocessors: {
-            [filesBootstrap]: ["webpack"],
-            [filesPattern]: ["webpack"],
-        },
-        reporters: ["progress", "mocha"],
-        webpack: produce(webpackConfig, (draft) => {
-            // TODO get rid of "karma-webpack" hanging workaround
-            // https://github.com/webpack-contrib/karma-webpack/issues/322#issuecomment-417862717
-            (draft.output = draft.output || {}).filename = "[name]";
-        }),
-        webpackServer: {noInfo: true},
-        port: 9876,
-        colors: true,
-        logLevel: "INFO",
-        browsers: [
-            "Chrome",
-        ],
-        customLaunchers,
-        mime: {
-            "text/x-typescript": ["ts"],
-        },
-    };
-
-    if (TRAVIS_OS_NAME in customLaunchers) {
-        configuration.browsers = [TRAVIS_OS_NAME];
-    }
-
     config.set({
         ...configuration,
         // TODO get rid of karma circular JSON stringifying error, see https://github.com/karma-runner/karma/issues/3154

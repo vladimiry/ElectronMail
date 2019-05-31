963fdfffed628b5d8a6899a21c8d11a5d4414b20
Vladimir Y <dev.vladimir.y@gmail.com>
2019-05-31 20:56:47 +0300

    spellcheck: prefer "en_US" locale if suitable

--- src/electron-main/spell-check/setup.ts
+++ src/electron-main/spell-check/setup.ts
@@ -1,5 +1,4 @@
 import _logger from "electron-log";
-import fastGlob from "fast-glob";
 import os from "os";
 import path from "path";
 import semver from "semver";
@@ -9,22 +8,31 @@ import {curryFunctionMembers, removeDuplicateItems} from "src/shared/util";
 
 const logger = curryFunctionMembers(_logger, "[src/electron-main/spell-check/setup]");
 
-const state: {
-    location: string | undefined;
-    extraLocales: Locale[];
-} = {
-    extraLocales: [],
-    location: process.env.HUNSPELL_DICTIONARIES,
-};
+function normalizeLocale(value: string): string {
+    return value.replace(/[^A-Za-z]/g, "_");
+}
 
 export let resolveSystemLocale: () => Promise<Locale> = async () => {
     const osLocaleModule = await import("os-locale");
 
     // hunspell requires the fully-qualified locale
     // so load local with help of "os-locale" module as it returns locale in "es_ES" format vs "es." returned by "os" module
-    const result: Locale = osLocaleModule
-        .sync()
-        .replace("-", "_");
+    let result: Locale = normalizeLocale(osLocaleModule.sync()) || "en_US";
+
+    // prefer "en_US" locale if OS locale is in "en"-group and respective dictionary available
+    if (result.toLowerCase().startsWith("en_") && !result.toLowerCase().startsWith("en_us")) {
+        const {getAvailableDictionaries} = await setup();
+        const dictionaries = getAvailableDictionaries();
+        const preferredLocale = (
+            dictionaries.find((value) => value.toLowerCase() === "en_us")
+            ||
+            dictionaries.find((value) => value.toLowerCase().startsWith("en_us"))
+        );
+        if (preferredLocale) {
+            logger.info(`"${preferredLocale}" locale got preferred over "${result}"`);
+            result = preferredLocale;
+        }
+    }
 
     // the LANG environment variable is how node spellchecker finds its default language:
     // https://github.com/atom/node-spellchecker/blob/59d2d5eee5785c4b34e9669cd5d987181d17c098/lib/spellchecker.js#L29
@@ -35,30 +43,64 @@ export let resolveSystemLocale: () => Promise<Locale> = async () => {
     // memoize the result
     resolveSystemLocale = async () => result;
 
+    logger.info(`Detected system/default locale: ${result}`);
+
     return result;
 };
 
 export let setup: () => Promise<{
-    getLocation: () => typeof state.location;
+    getLocation: () => string | undefined;
     getAvailableDictionaries: () => readonly Locale[];
 }> = async () => {
+    const state: {
+        location: string | undefined;
+        extraLocales: Locale[];
+    } = {
+        extraLocales: [],
+        location: process.env.HUNSPELL_DICTIONARIES,
+    };
     const platform = os.platform();
-    const systemLocale = await resolveSystemLocale();
 
     if (platform === "linux") {
-        setupLinux(systemLocale);
+        await (async () => {
+            const fastGlobModule = await import("fast-glob");
+
+            // apt-get install hunspell-<locale> can be run for easy access to other dictionaries
+            state.location = state.location || "/usr/share/hunspell";
+
+            // hunspell"s "getAvailableDictionaries" does nothing, so use glob resolving as a workaround
+            const dictionaryFiles = await fastGlobModule.async<string>(
+                // result is array of strings is "stats" option disabled (default behaviour)
+                path.join(state.location, "*.dic"),
+            );
+            const dictionaryLocales = dictionaryFiles.map((dictionaryFile) => {
+                return normalizeLocale(
+                    path.basename(dictionaryFile)
+                        .replace(".dic", ""),
+                );
+            });
+
+            logger.info("Found hunspell dictionaries", dictionaryLocales);
+            state.extraLocales.push(...dictionaryLocales);
+
+            logger.info(`Detected Linux. Dictionary location: ${state.location}`);
+        })();
     } else if (platform === "win32" && semver.lt(os.release(), "8.0.0")) {
-        setupWin7AndEarlier(systemLocale);
+        logger.info(`Detected Windows 7 or below. Dictionary location: ${state.location}`);
     } else {
         // OSX and Windows 8+ have OS-level spellcheck APIs
-        logger.info("Using OS-level spell check API with locale", systemLocale);
+        logger.info(`Detected OSX/Windows 8+. Using OS-level spell check API`);
     }
 
     const spellCheckerModule = await import("spellchecker");
-    const availableDictionaries: readonly Locale[] = removeDuplicateItems([
-        ...spellCheckerModule.getAvailableDictionaries(),
-        ...state.extraLocales,
-    ]);
+    const availableDictionaries: readonly Locale[] = removeDuplicateItems(
+        [
+            ...spellCheckerModule.getAvailableDictionaries(),
+            // WARN: this needs to be called after OS-dependent initialization got completed (see above code lines)
+            // ie "state" got settled down
+            ...state.extraLocales,
+        ].map(normalizeLocale),
+    );
     const result = {
         getLocation() {
             return state.location;
@@ -73,50 +115,3 @@ export let setup: () => Promise<{
 
     return result;
 };
-
-function setupLinux(locale: Locale) {
-    // apt-get install hunspell-<locale> can be run for easy access to other dictionaries
-    state.location = state.location || "/usr/share/hunspell";
-
-    // hunspell"s "getAvailableDictionaries" does nothing, so use glob resolving as a workaround
-    const dictionaryFiles = fastGlob.sync<string>(
-        // result is array of strings is "stats" option disabled (default behaviour)
-        path.join(state.location, "*.dic"),
-    );
-    const locales = dictionaryFiles.map((dictionaryFile) => {
-        return path.basename(dictionaryFile)
-            .replace(".dic", "")
-            .replace("-", "_");
-    });
-
-    logger.info("Found hunspell dictionaries", locales);
-    state.extraLocales.push(...locales);
-
-    if (process.env.HUNSPELL_DICTIONARIES || locale !== "en_US") {
-        logger.info(
-            "Detected Linux. Setting up spell check with locale",
-            locale,
-            "and dictionary location",
-            location,
-        );
-    } else {
-        logger.info(
-            "Detected Linux. Using default en_US spell check dictionary",
-        );
-    }
-}
-
-function setupWin7AndEarlier(locale: Locale) {
-    if (state.location || locale !== "en_US") {
-        logger.info(
-            "Detected Windows 7 or below. Setting up spell-check with locale",
-            locale,
-            "and dictionary location",
-            state.location,
-        );
-    } else {
-        logger.info(
-            "Detected Windows 7 or below. Using default en_US spell check dictionary",
-        );
-    }
-}

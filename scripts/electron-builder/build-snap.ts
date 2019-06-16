6c7c967b71b8efa238d407be133d63370b2e74b4
Vladimir Y <dev.vladimir.y@gmail.com>
2019-06-17 01:01:22 +0300

    no need to explicitly disable sandbox for snap package anymore
    * as "electron-builder" does that since 20.44.1 release

--- scripts/electron-builder/build-snap.ts
+++ scripts/electron-builder/build-snap.ts
@@ -1,11 +1,10 @@
-import fs from "fs";
 import path from "path";
 import {Packager, Platform} from "app-builder-lib";
 import {getConfig} from "app-builder-lib/out/util/config";
 
 import {APP_EXEC_PATH_RELATIVE_HUNSPELL_DIR, BINARY_NAME} from "src/shared/constants";
-import {CWD, LOG, LOG_LEVELS, execShell} from "scripts/lib";
-import {DISABLE_SANDBOX_ARGS_LINE, copyDictionaryFilesTo, ensureFileHasNoSuidBit} from "scripts/electron-builder/lib";
+import {CWD, LOG, execShell} from "scripts/lib";
+import {copyDictionaryFilesTo, ensureFileHasNoSuidBit} from "scripts/electron-builder/lib";
 
 (async () => {
     await postProcess(
@@ -39,30 +38,8 @@ async function postProcess({packageFile}: { packageFile: string }) {
 
     await unpack({packageDir, packageFile});
     await copyDictionaryFilesTo(path.join(packageDir, APP_EXEC_PATH_RELATIVE_HUNSPELL_DIR));
-    disableSandbox({packageDir});
     ensureFileHasNoSuidBit(path.join(packageDir, BINARY_NAME));
     await packAndCleanup({packageDir, packageFile});
-}
-
-function disableSandbox({packageDir}: { packageDir: string; }): void {
-    const shFile = path.join(packageDir, "./command.sh");
-    const shContentOriginal = fs.readFileSync(shFile).toString();
-    const shContentPatched = (() => {
-        const searchValue = `exec $SNAP/bin/desktop-launch "$SNAP/${BINARY_NAME}"`;
-        const replaceWith = `${searchValue} ${DISABLE_SANDBOX_ARGS_LINE} $@`;
-        return shContentOriginal.replace(searchValue, replaceWith);
-    })();
-
-    if (shContentPatched === shContentOriginal) {
-        throw new Error(`Failed to patch content of the "${shFile}" file`);
-    }
-
-    LOG(
-        LOG_LEVELS.title(`Writing ${LOG_LEVELS.value(shFile)} file with content:`),
-        LOG_LEVELS.value(shContentPatched),
-    );
-
-    fs.writeFileSync(shFile, shContentPatched);
 }
 
 async function unpack({packageFile, packageDir}: { packageFile: string; packageDir: string; }) {

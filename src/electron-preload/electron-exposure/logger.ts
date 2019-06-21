7aea54323af9a1417e36b429c30e7ba6b8a13d5d
Vladimir Y <dev.vladimir.y@gmail.com>
2019-06-21 12:54:09 +0300

    fix log lines not being written from renderer processes in some cases
    * there was exact log level comparison but should be weight-based
    * the bug arrived with https://github.com/vladimiry/ElectronMail/issues/152 change / 83ddc3fa794e8e775f4d035bf58c18315785de8d

--- src/electron-preload/electron-exposure/logger.ts
+++ src/electron-preload/electron-exposure/logger.ts
@@ -2,15 +2,9 @@ import {IFileTransport} from "electron-log"; // tslint:disable-line:no-import-zo
 import {remote} from "electron"; // tslint:disable-line:no-import-zones
 
 import {Logger} from "src/shared/model/common";
+import {ReadonlyDeep} from "type-fest"; // tslint:disable-line:no-import-zones
 
-const logger: Logger & { transports: { file: IFileTransport } } = remote.require("electron-log");
-
-function log(level: keyof Logger, ...params: any[]): void {
-    if (logger.transports.file.level !== level) {
-        return;
-    }
-    logger[level](...params);
-}
+const logger: ReadonlyDeep<Logger & { transports: { file: IFileTransport } }> = remote.require("electron-log");
 
 export const LOGGER: Logger = {
     error: log.bind(null, "error"),
@@ -20,3 +14,27 @@ export const LOGGER: Logger = {
     debug: log.bind(null, "debug"),
     silly: log.bind(null, "silly"),
 };
+
+const LOGGER_LEVELS_WEIGHT: Readonly<Record<keyof Logger | "null" | "undefined" | "false", number>> = {
+    null: -1,
+    undefined: -1,
+    false: -1,
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4,
+    silly: 5,
+};
+
+function log(level: keyof Logger, ...params: any[]): void {
+    if (
+        LOGGER_LEVELS_WEIGHT[level]
+        >
+        LOGGER_LEVELS_WEIGHT[String(logger.transports.file.level) as keyof typeof LOGGER_LEVELS_WEIGHT]
+    ) {
+        return;
+    }
+
+    logger[level](...params);
+}

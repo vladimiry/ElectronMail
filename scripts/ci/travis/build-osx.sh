e9aa64ec56371cd5af40e9c1841325c91ce5c610
Vladimir Y <dev.vladimir.y@gmail.com>
2020-01-29 07:55:30 +0300

    travis ci: get back missed "scripts/ci/travis/build-osx.sh" script
    * was removed before in 5342bbd8e40d2d61e0e4c45f189c79ffce0a880d

---
+++ scripts/ci/travis/build-osx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ev
+
+./scripts/ci/prepare-webclients.sh
+
+yarn app:dist
+
+yarn test:e2e
+
+# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+# output something every 9 minutes (540 seconds) to prevent Travis killing the job
+while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+yarn electron-builder:dist
+# killing background sleep loop
+kill %1
+
+yarn scripts/dist-packages/print-hashes
+yarn scripts/dist-packages/upload

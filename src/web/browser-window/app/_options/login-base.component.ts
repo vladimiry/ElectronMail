066394fcee6ab3ee0017565fedacbe9e730d0697
Vladimir Y <dev.vladimir.y@gmail.com>
2020-02-05 03:18:37 +0300

    enable addition master password saving warning

--- src/web/browser-window/app/_options/login-base.component.ts
+++ src/web/browser-window/app/_options/login-base.component.ts
@@ -2,10 +2,10 @@ import {AfterViewInit, ElementRef, Injector, OnDestroy, QueryList, ViewChildren}
 import {FormControl, Validators} from "@angular/forms";
 import {Observable, Subscription} from "rxjs";
 import {Store, select} from "@ngrx/store";
-import {map} from "rxjs/operators";
+import {filter, map, pairwise} from "rxjs/operators";
 
+import {NOTIFICATION_ACTIONS, OPTIONS_ACTIONS} from "src/web/browser-window/app/store/actions";
 import {ONE_SECOND_MS} from "src/shared/constants";
-import {OPTIONS_ACTIONS} from "src/web/browser-window/app/store/actions";
 import {OptionsSelectors} from "src/web/browser-window/app/store/selectors";
 import {State} from "src/web/browser-window/app/store/reducers/options";
 
@@ -46,6 +46,8 @@ export abstract class LoginBaseComponent implements AfterViewInit, OnDestroy {
                 this.savePassword[value ? "enable" : "disable"]();
             }),
         );
+
+        this.enableMasterPasswordWarning();
     }
 
     ngAfterViewInit() {
@@ -63,5 +65,23 @@ export abstract class LoginBaseComponent implements AfterViewInit, OnDestroy {
 
     ngOnDestroy() {
         this.subscription.unsubscribe();
+    }
+
+    private enableMasterPasswordWarning() {
+        this.subscription.add(
+            this.savePassword.valueChanges.pipe(
+                pairwise(),
+                filter(([prev, curr]) => curr && Boolean(curr) !== Boolean(prev)),
+            ).subscribe(() => {
+                this.store.dispatch(
+                    NOTIFICATION_ACTIONS.ErrorMessage({
+                        message: "Saving the master password on computer weakens the security.",
+                    }),
+                );
+            }),
+        );
+
+        // making sure "pairwise()" to be triggered on first value changing to "true"
+        this.savePassword.patchValue(this.savePassword.value);
     }
 }

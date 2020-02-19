d09ba55c493048f8080ca8dc483fd46b4fe4d382
Vladimir Y <dev.vladimir.y@gmail.com>
2020-02-19 21:47:17 +0300

    tweak ci jobs
    * looks like "Ubuntu1804" on Appveyor job needs to be split into pieces

--- scripts/ci/prepare-webclients.sh
+++ scripts/ci/prepare-webclients.sh
@@ -8,7 +8,13 @@ set -ev
 ARCHIVE_FILE=webclients.tar
 
 # build
-yarn assets:webclient:protonmail
+# preventing "No output has been received in the last 10m0s" error occuring on travis-ci
+# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+# output something every 9 minutes (540 seconds) to prevent Travis killing the job
+while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    yarn assets:webclient:protonmail
+# killing background sleep loop
+kill %1
 
 # create archive
 ./scripts/ci/archive-webclients-dist-only.sh $ARCHIVE_FILE
--- scripts/ci/travis/build-osx.sh
+++ scripts/ci/travis/build-osx.sh
@@ -8,10 +8,11 @@ yarn app:dist
 
 yarn test:e2e
 
+# preventing "No output has been received in the last 10m0s" error occuring on travis-ci
 # see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
 # output something every 9 minutes (540 seconds) to prevent Travis killing the job
 while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
-yarn electron-builder:dist
+    yarn electron-builder:dist
 # killing background sleep loop
 kill %1
 

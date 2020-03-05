cda5d9534f4ed0992596c7c8688b2d6892ae937e
Vladimir Y <dev.vladimir.y@gmail.com>
2020-03-05 22:41:55 +0300

    add "snapcraft" lib in "electronuserland/builder" docker image use flow
    * https://travis-ci.org/vladimiry/ElectronMail/jobs/658824422#L5240
    * TODO use own docker image instead of "electronuserland/builder"

--- scripts/ci/travis/build-linux.sh
+++ scripts/ci/travis/build-linux.sh
@@ -23,7 +23,7 @@ docker run --rm -ti \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/builder \
-    /bin/bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && export NVM_DIR=~/.nvm && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION && nvm use $TRAVIS_NODE_VERSION && apt-get update && apt-get install --yes --no-install-recommends libtool automake squashfs-tools libxss-dev && yarn --pure-lockfile && yarn clean:prebuilds && yarn scripts/electron-builder/sequential-dist-linux"
+    /bin/bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && export NVM_DIR=~/.nvm && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION && nvm use $TRAVIS_NODE_VERSION && apt-get update && apt-get install --yes --no-install-recommends libtool automake squashfs-tools libxss-dev snapcraft && yarn --pure-lockfile && yarn clean:prebuilds && yarn scripts/electron-builder/sequential-dist-linux"
 
 yarn scripts/dist-packages/print-hashes
 yarn scripts/dist-packages/upload

1a9ad60385aecf56c1f02154c5298cf24ca385d2
Vladimir Y <dev.vladimir.y@gmail.com>
2020-03-16 12:08:58 +0300

    get rid of "packages" folder
    * "packages/import-sort-style" project got moved to "@vladimiry/import-sort-style" package

--- packages/import-sort-style/README.md
+++
@@ -1,1 +0,0 @@
-Custom [import-sort](https://github.com/renke/import-sort) style rule built for use by [ElectronMail](https://github.com/vladimiry/ElectronMail).
--- packages/import-sort-style/test/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-
-import _ from "./../dist";
-
-// TODO implement proper tests
-
-test(`"default" import should be a function`, (t) => {
-    t.is("function", typeof _);
-});

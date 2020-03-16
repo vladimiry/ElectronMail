0c050acc8d8ff0dcb1bbdf22e3661e0c50bd08b5
Vladimir Y <dev.vladimir.y@gmail.com>
2020-03-16 16:53:48 +0300

    update "fs-json-store-encryption-adapter" dependency

--- src/electron-main/__test__/api/index.spec.ts
+++ src/electron-main/__test__/api/index.spec.ts
@@ -580,8 +580,10 @@ test.beforeEach(async (t) => {
     memFsVolume._impl.writeFileSync(path.join(appDir, "web/browser-window/shared-vendor.css"), "");
 
     // reducing work factor in order to speed-up the test process and make it less computing resources consuming
-    encryptionPreset.keyDerivation = {type: "sodium.crypto_pwhash", preset: "mode:interactive|algorithm:default"};
-    encryptionPreset.encryption = {type: "crypto", preset: "algorithm:aes-256-cbc"};
+    (encryptionPreset as import("type-fest").Mutable<typeof encryptionPreset>).keyDerivation
+        = {type: "sodium.crypto_pwhash", preset: "mode:interactive|algorithm:default"};
+    (encryptionPreset as import("type-fest").Mutable<typeof encryptionPreset>).encryption
+        = {type: "crypto", preset: "algorithm:aes-256-cbc"};
 
     logger.transports.file = ((msg: any) => {
         logger.transports.console(msg);

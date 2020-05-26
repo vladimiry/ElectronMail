bc0ed0287637be493623ac882a6a9340b0b4004d
Vladimir Y <dev.vladimir.y@gmail.com>
2020-05-26 10:03:54 +0300

    fix months number value in exported file names

--- src/electron-main/api/endpoints-builders/database/export.ts
+++ src/electron-main/api/endpoints-builders/database/export.ts
@@ -31,7 +31,7 @@ const padStart = (value: number, args: [number, string] = [2, "0"]): string => {
 
 const buildSortableDate = (date: Date): string => {
     const year = date.getFullYear();
-    const month = padStart(date.getMonth());
+    const month = padStart(date.getMonth() + 1);
     const day = padStart(date.getDate());
     const hours = padStart(date.getHours());
     const minutes = padStart(date.getMinutes());

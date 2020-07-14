ca7698a15d397da94d28c1e65a6d3edce90153b6
Vladimir Y <dev.vladimir.y@gmail.com>
2020-07-14 15:43:07 +0300

    prevent "no output has been received in the last 10m0s" travis ci issue

--- scripts/ci/travis/build-linux.sh
+++ scripts/ci/travis/build-linux.sh
@@ -26,4 +26,11 @@ docker run --rm -ti \
     /bin/bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && export NVM_DIR=~/.nvm && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION && nvm use $TRAVIS_NODE_VERSION && apt-get update && apt-get install --yes --no-install-recommends libtool automake squashfs-tools libxss-dev snapcraft && yarn --pure-lockfile && yarn clean:prebuilds && yarn scripts/electron-builder/sequential-dist-linux"
 
 yarn scripts/dist-packages/print-hashes
-yarn scripts/dist-packages/upload
+
+# preventing "No output has been received in the last 10m0s" error occurring on travis-ci
+# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+# output something every 9 minutes (540 seconds) to prevent Travis killing the job
+while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    yarn scripts/dist-packages/upload
+# killing background sleep loop
+kill %1
--- scripts/ci/travis/build-osx.sh
+++ scripts/ci/travis/build-osx.sh
@@ -8,7 +8,7 @@ yarn app:dist
 
 yarn test:e2e
 
-# preventing "No output has been received in the last 10m0s" error occuring on travis-ci
+# preventing "No output has been received in the last 10m0s" error occurring on travis-ci
 # see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
 # output something every 9 minutes (540 seconds) to prevent Travis killing the job
 while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
@@ -17,4 +17,11 @@ while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
 kill %1
 
 yarn scripts/dist-packages/print-hashes
-yarn scripts/dist-packages/upload
+
+# preventing "No output has been received in the last 10m0s" error occurring on travis-ci
+# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+# output something every 9 minutes (540 seconds) to prevent Travis killing the job
+while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    yarn scripts/dist-packages/upload
+# killing background sleep loop
+kill %1

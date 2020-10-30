8e854bf0c891959a84590d4525977bb3aca8c4f7
Vladimir Y <dev.vladimir.y@gmail.com>
2020-10-30 15:46:42 +0300

    slightly simplify "account-view-primary" component

--- src/web/browser-window/app/_accounts/account-view-primary.component.ts
+++ src/web/browser-window/app/_accounts/account-view-primary.component.ts
@@ -10,14 +10,6 @@ import {AccountsService} from "src/web/browser-window/app/_accounts/accounts.ser
 import {getZoneNameBoundWebLogger} from "src/web/browser-window/util";
 import {testProtonAppPage} from "src/shared/util";
 
-const pickCredentialFields = ((fields: Array<keyof AccountConfig>) => {
-    return (accountConfig: AccountConfig) => pick(accountConfig, fields);
-})(["credentials"]);
-
-const pickLoginDelayFields = ((fields: Array<keyof AccountConfig>) => {
-    return (accountConfig: AccountConfig) => pick(accountConfig, fields);
-})(["loginDelayUntilSelected", "loginDelaySecondsRange"]);
-
 @Component({
     selector: "electron-mail-account-view-primary",
     templateUrl: "./account-view-primary.component.html",
@@ -45,21 +37,16 @@ export class AccountViewPrimaryComponent extends AccountViewAbstractComponent im
 
         this.addSubscription(
             this.filterDomReadyEvent().subscribe(({webView}) => {
-                {
-                    const {src: url} = webView;
-                    const {shouldInitProviderApi} = testProtonAppPage({url, logger: this.logger});
-
-                    if (!shouldInitProviderApi) {
-                        this.event.emit({
-                            type: "log",
-                            data: ["info", `skip webview.dom-ready processing for ${url} page`]
-                        });
-                        this.event.emit({
-                            type: "action",
-                            payload: this.accountsService.generateNotificationsStateResetAction({login: this.account.accountConfig.login}),
-                        });
-                        return;
-                    }
+                if (!testProtonAppPage({url: webView.src, logger: this.logger}).shouldInitProviderApi) {
+                    this.event.emit({
+                        type: "log",
+                        data: ["info", `skip webview.dom-ready processing for ${webView.src} page`]
+                    });
+                    this.event.emit({
+                        type: "action",
+                        payload: this.accountsService.generateNotificationsStateResetAction({login: this.account.accountConfig.login}),
+                    });
+                    return;
                 }
 
                 const finishPromise = this.filterDomReadyOrDestroyedPromise();
@@ -80,21 +67,23 @@ export class AccountViewPrimaryComponent extends AccountViewAbstractComponent im
                         distinctUntilChanged((prev, curr) => equals(prev.data, curr.data)),
                         takeUntil(from(finishPromise)),
                     )
-                    .subscribe(({pk, data}) => {
+                    .subscribe(({pk, data: {loggedIn, database}}) => {
                         breakPreviousSyncing$.next();
 
-                        if (!data.loggedIn || !data.database) {
+                        if (!loggedIn || !database) {
                             return; // syncing disabled
                         }
 
-                        const syncingFinishPromise = race([
-                            from(finishPromise),
-                            breakPreviousSyncing$.pipe(take(1)),
-                        ]).toPromise().then(() => {}); // eslint-disable-line @typescript-eslint/no-empty-function
-
                         this.event.emit({
                             type: "action",
-                            payload: ACCOUNTS_ACTIONS.ToggleSyncing({pk, webView, finishPromise: syncingFinishPromise}),
+                            payload: ACCOUNTS_ACTIONS.ToggleSyncing({
+                                pk,
+                                webView,
+                                finishPromise: race([
+                                    from(finishPromise),
+                                    breakPreviousSyncing$.pipe(take(1)),
+                                ]).toPromise(),
+                            }),
                         });
                     });
             }),
@@ -102,6 +91,9 @@ export class AccountViewPrimaryComponent extends AccountViewAbstractComponent im
     }
 
     private onWebViewDomReadyOnceHandler(webView: Electron.WebviewTag): void {
+        const pickCredentialFields = (value: AccountConfig) => pick(value, ["credentials"]);
+        const pickLoginDelayFields = (value: AccountConfig) => pick(value, ["loginDelayUntilSelected", "loginDelaySecondsRange"]);
+
         this.addSubscription(
             this.account$.pipe(
                 pairwise(),
@@ -109,13 +101,15 @@ export class AccountViewPrimaryComponent extends AccountViewAbstractComponent im
                     return (
                         // page changed: "entryUrl" is changeable
                         // so need to react on "url" change too (deep comparison with "equals")
-                        curr.notifications.pageType.type !== "unknown"
-                        &&
-                        !equals(prev.notifications.pageType, curr.notifications.pageType)
-                    ) || (
-                        // creds changed
+                        (
+                            curr.notifications.pageType.type !== "unknown"
+                            &&
+                            !equals(prev.notifications.pageType, curr.notifications.pageType)
+                        )
+                        ||
+                        // credentials changed
                         !equals(pickCredentialFields(prev.accountConfig), pickCredentialFields(curr.accountConfig))
-                    ) || (
+                        ||
                         // login delay values changed
                         !equals(pickLoginDelayFields(prev.accountConfig), pickLoginDelayFields(curr.accountConfig))
                     );

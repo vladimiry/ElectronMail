b114cddfcdbac336a03598a5bc3af536cfc8179e
Vladimir Y <dev.vladimir.y@gmail.com>
2020-11-17 07:43:40 +0300

    explicitly allow devtools opening only in dev build mode

--- src/electron-main/window/constants.ts
+++ src/electron-main/window/constants.ts
@@ -2,19 +2,21 @@ import {WebPreferences} from "electron";
 
 export const DEFAULT_WEB_PREFERENCES_KEYS = [
     "backgroundThrottling",
+    "devTools",
     "disableBlinkFeatures",
+    "enableRemoteModule",
     "nodeIntegration",
     "nodeIntegrationInWorker",
     "sandbox",
     "spellcheck",
     "webSecurity",
     "webviewTag",
-    "enableRemoteModule",
 ] as const;
 
 export const DEFAULT_WEB_PREFERENCES: Readonly<NoExtraProps<Pick<Required<WebPreferences>, typeof DEFAULT_WEB_PREFERENCES_KEYS[number]>>>
     = {
     backgroundThrottling: false,
+    devTools: BUILD_ENVIRONMENT === "development",
     disableBlinkFeatures: "Auxclick",
     nodeIntegration: false,
     nodeIntegrationInWorker: false,

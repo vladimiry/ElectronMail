a83f136c509576ee9f772edd51a6f83105744d78
Vladimir Y <1560781+vladimiry@users.noreply.github.com>
2020-12-12 13:02:35 +0300

    continuous integration: limit build stages for pull request trigger

--- scripts/ci/travis/build-linux.sh
+++ scripts/ci/travis/build-linux.sh
@@ -2,25 +2,30 @@
 
 set -ev
 
-yarn app:dist:base
-npm run clean:prebuilds
-npx --no-install electron-builder install-app-deps --arch=x64
+yarn app:dist:build
 
-# needed for e2e starts with SUID sandbox if user namespacing disabled
-# https://github.com/electron/electron/issues/16631#issuecomment-476082063
-# https://github.com/electron/electron/issues/17972
-sudo ./scripts/prepare-chrome-sandbox.sh ./node_modules/electron/dist/chrome-sandbox
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    yarn assets
 
-yarn test:e2e
+    npm run clean:prebuilds
+    npx --no-install electron-builder install-app-deps --arch=x64
 
-# --env-file: https://github.com/electron-userland/electron-builder/issues/2450
+    # needed for e2e starts with SUID sandbox if user namespacing disabled
+    # https://github.com/electron/electron/issues/16631#issuecomment-476082063
+    # https://github.com/electron/electron/issues/17972
+    sudo ./scripts/prepare-chrome-sandbox.sh ./node_modules/electron/dist/chrome-sandbox
 
-# TODO use own docker image
-docker run --rm -ti \
-    --env-file <(env | grep -vE '\r|\n' | grep -iE '^(DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR|BUILD_)[A-Z_]*=') \
-    -v ${PWD}:/project \
-    -v ${PWD##*/}-node-modules:/project/node_modules \
-    -v ~/.cache/electron:/root/.cache/electron \
-    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-    electronuserland/builder \
-    /bin/bash ./scripts/ci/travis/build-linux-docker.sh
+    yarn test:e2e
+
+    # --env-file: https://github.com/electron-userland/electron-builder/issues/2450
+
+    # TODO use own docker image
+    docker run --rm -ti \
+        --env-file <(env | grep -vE '\r|\n' | grep -iE '^(DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR|BUILD_)[A-Z_]*=') \
+        -v ${PWD}:/project \
+        -v ${PWD##*/}-node-modules:/project/node_modules \
+        -v ~/.cache/electron:/root/.cache/electron \
+        -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+        electronuserland/builder \
+        /bin/bash ./scripts/ci/travis/build-linux-docker.sh
+fi
--- scripts/ci/travis/build-osx.sh
+++ scripts/ci/travis/build-osx.sh
@@ -2,26 +2,31 @@
 
 set -ev
 
-yarn app:dist:base
-npm run clean:prebuilds
-npx --no-install electron-builder install-app-deps --arch=x64
+yarn app:dist:build
 
-yarn test:e2e
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    yarn assets
 
-# preventing "No output has been received in the last 10m0s" error occurring on travis-ci
-# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
-# output something every 9 minutes (540 seconds) to prevent Travis killing the job
-while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    npm run clean:prebuilds
+    npx --no-install electron-builder install-app-deps --arch=x64
+
+    yarn test:e2e
+
+    # preventing "No output has been received in the last 10m0s" error occurring on travis-ci
+    # see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+    # output something every 9 minutes (540 seconds) to prevent Travis killing the job
+    while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
     yarn electron-builder:dist
-# killing background sleep loop
-kill %1
+    # killing background sleep loop
+    kill %1
 
-yarn scripts/dist-packages/print-hashes
+    yarn scripts/dist-packages/print-hashes
 
-# preventing "No output has been received in the last 10m0s" error occurring on travis-ci
-# see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
-# output something every 9 minutes (540 seconds) to prevent Travis killing the job
-while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    # preventing "No output has been received in the last 10m0s" error occurring on travis-ci
+    # see https://github.com/travis-ci/travis-ci/issues/4190#issuecomment-353342526
+    # output something every 9 minutes (540 seconds) to prevent Travis killing the job
+    while sleep 540; do echo "=====[ $SECONDS seconds still running ]====="; done &
     yarn scripts/dist-packages/upload
-# killing background sleep loop
-kill %1
+    # killing background sleep loop
+    kill %1
+fi

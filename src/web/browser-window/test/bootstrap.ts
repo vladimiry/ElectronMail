d39b1bfb91b068e7b4a7172f1e8d98d5ad09f227
Vladimir Y <1560781+vladimiry@users.noreply.github.com>
2021-04-07 22:02:39 +0300

    drop "web" test

--- src/web/browser-window/test/bootstrap.ts
+++
@@ -1,9 +0,0 @@
-import "zone.js/dist/zone";
-import "zone.js/dist/long-stack-trace-zone";
-import "zone.js/dist/async-test";
-import "zone.js/dist/fake-async-test";
-import "zone.js/dist/sync-test";
-import "zone.js/dist/proxy";
-import "zone.js/dist/jasmine-patch";
-
-Error.stackTraceLimit = Infinity;

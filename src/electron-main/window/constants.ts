49fbb359d0644ab7b88f5b7bb2402b4cfde1e714
Vladimir Y <1560781+vladimiry@users.noreply.github.com>
2021-07-06 21:20:09 +0300

    enable @electron "webPreferences.nativeWindowOpen" flag
    * Enabled by default since @electron v15.

--- src/electron-main/window/constants.ts
+++ src/electron-main/window/constants.ts
@@ -5,6 +5,7 @@ export const DEFAULT_WEB_PREFERENCES_KEYS = [
     "contextIsolation",
     "devTools",
     "disableBlinkFeatures",
+    "nativeWindowOpen",
     "nodeIntegration",
     "nodeIntegrationInWorker",
     "sandbox",
@@ -16,10 +17,10 @@ export const DEFAULT_WEB_PREFERENCES_KEYS = [
 export const DEFAULT_WEB_PREFERENCES: Readonly<NoExtraProps<Pick<Required<WebPreferences>, typeof DEFAULT_WEB_PREFERENCES_KEYS[number]>>>
     = {
     backgroundThrottling: false,
-    // TODO disable "contextIsolation" flag
-    contextIsolation: false,
+    contextIsolation: false, // TODO disable "contextIsolation" flag
     devTools: BUILD_ENVIRONMENT !== "production",
     disableBlinkFeatures: "Auxclick",
+    nativeWindowOpen: true,
     nodeIntegration: false,
     nodeIntegrationInWorker: false,
     sandbox: true,

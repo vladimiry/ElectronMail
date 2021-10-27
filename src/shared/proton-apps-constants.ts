0133434c1950832e76c7487df6ec31f09bd1a0c4
Vladimir Y <1560781+vladimiry@users.noreply.github.com>
2021-10-27 19:37:13 +0300

    update @ProtonMail web clients

--- src/shared/proton-apps-constants.ts
+++ src/shared/proton-apps-constants.ts
@@ -20,7 +20,7 @@ export const PROVIDER_REPO_MAP = {
     [PROVIDER_APP_NAMES[0]]: {
         repoRelativeDistDir: "./dist",
         baseDirName: "",
-        tag: "proton-mail@4.10.0",
+        tag: "proton-mail@4.11.1",
         protonPack: {
             appConfig: {clientId: "WebMail"},
             webpackIndexEntryItems: [
@@ -48,13 +48,13 @@ export const PROVIDER_REPO_MAP = {
     [PROVIDER_APP_NAMES[1]]: {
         repoRelativeDistDir: "./dist",
         baseDirName: "account",
-        tag: "proton-account@4.12.1",
+        tag: "proton-account@4.12.8",
         protonPack: {appConfig: {clientId: "WebAccount"}}
     },
     [PROVIDER_APP_NAMES[2]]: {
         repoRelativeDistDir: "./dist",
         baseDirName: "calendar",
-        tag: "proton-calendar@4.7.0",
+        tag: "proton-calendar@4.7.6",
         protonPack: {
             appConfig: {clientId: "WebCalendar"},
             webpackIndexEntryItems: [
@@ -67,7 +67,7 @@ export const PROVIDER_REPO_MAP = {
     [PROVIDER_APP_NAMES[3]]: {
         repoRelativeDistDir: "./dist",
         baseDirName: "drive",
-        tag: "proton-drive@4.6.0",
+        tag: "proton-drive@4.6.1",
         protonPack: {appConfig: {clientId: "WebDrive"}},
     },
 } as const;

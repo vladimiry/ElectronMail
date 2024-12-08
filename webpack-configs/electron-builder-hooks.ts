79cca6cfb7b7bb04b0473f94b36ed642d957cc81
VL.Y <1560781+vladimiry@users.noreply.github.com>
2024-12-09 02:02:31 +0300

    enable extra measure for removing "prebuilds"

--- webpack-configs/electron-builder-hooks.ts
+++ webpack-configs/electron-builder-hooks.ts
@@ -1,30 +1,33 @@
 import {Configuration} from "webpack";
+import fastGlob from "fast-glob";
 import nodeExternals from "webpack-node-externals";
 import path from "path";
 
 import {buildBaseConfig, rootRelativePath, typescriptLoaderRule} from "./lib";
+import {CONSOLE_LOG} from "scripts/lib";
+import {sanitizeFastGlobPattern} from "src/shared/util/sanitize";
 
-const hooksDir = (...value: string[]): string => {
-    return path.join(rootRelativePath("./scripts/electron-builder/hooks"), ...value);
+const hooksDir = rootRelativePath("./scripts/electron-builder");
+const tsConfigFile = path.join(hooksDir, "tsconfig.json");
+
+export default async (): Promise<Configuration[]> => {
+    const hookSrcFiles = await fastGlob(
+        sanitizeFastGlobPattern(`${hooksDir}/hook-*.ts`),
+        {deep: 1, onlyFiles: true, stats: false},
+    );
+
+    CONSOLE_LOG(`Delected hook src files: ${JSON.stringify(hookSrcFiles, null, 2)}`);
+
+    return hookSrcFiles.map((hookSrcFile) => {
+        return buildBaseConfig({
+            mode: "none",
+            devtool: false,
+            target: "node",
+            entry: {[path.basename(hookSrcFile, ".ts")]: hookSrcFile},
+            output: {path: hooksDir, libraryTarget: "commonjs2", libraryExport: "default", filename: "[name].cjs"},
+            module: {rules: [typescriptLoaderRule({tsConfigFile})]},
+            externals: [nodeExternals()],
+            resolve: {alias: {scripts: rootRelativePath("scripts")}},
+        }, {tsConfigFile});
+    });
 };
-
-// TODO scan folder automatically
-const hooksToBuild = ["afterPack"] as const;
-
-const configurations: Configuration[] = hooksToBuild.map((hookDirName) => {
-    const hookDir = path.join(hooksDir(hookDirName));
-    const tsConfigFile = path.join(hookDir, "tsconfig.json");
-
-    return buildBaseConfig({
-        mode: "none",
-        devtool: false,
-        target: "node",
-        entry: {index: hookDir},
-        output: {path: hookDir, libraryTarget: "commonjs2", libraryExport: "default", filename: "[name].cjs"},
-        module: {rules: [typescriptLoaderRule({tsConfigFile})]},
-        externals: [nodeExternals()],
-        resolve: {alias: {scripts: rootRelativePath("scripts")}},
-    }, {tsConfigFile});
-});
-
-export default configurations;

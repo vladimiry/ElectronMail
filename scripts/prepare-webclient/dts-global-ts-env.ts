5d7914947baf7f205cad3311773e2ad3897c0224
VL.Y <1560781+vladimiry@users.noreply.github.com>
2025-07-28 02:00:21 +0300

    code-based search: include more typescript libs in "monaco-editor" env
    * so it better represents the "quickjs-emscripten" capabilities in the "monaco-editor"-based code editor

--- scripts/prepare-webclient/dts-global-ts-env.ts
+++ scripts/prepare-webclient/dts-global-ts-env.ts
@@ -8,6 +8,13 @@ const LIBS_TO_INCLUDE = [
     "es5",
     "es2015.core",
     "es2015.collection",
+    "es2015.promise",
+    "es2015.symbol",
+    "es2016.array.include",
+    "es2017.object",
+    "es2018.promise",
+    "es2019.object",
+    "es2020",
 ];
 
 export const generateGlobalTypescriptEnvDeclaration = (libDir: string, destFile: string): void => {
@@ -19,6 +26,8 @@ export const generateGlobalTypescriptEnvDeclaration = (libDir: string, destFile:
     const visited = new Set<string>();
     const output: string[] = [];
 
+    CONSOLE_LOG(`creating "${destFile}"`);
+
     LIBS_TO_INCLUDE.forEach(addLibFile);
 
     fsExtra.ensureDirSync(path.dirname(destFile));
@@ -28,6 +37,9 @@ export const generateGlobalTypescriptEnvDeclaration = (libDir: string, destFile:
 
     function addLibFile(libName: string): void {
         const fileName = path.join(libDir, `lib.${libName}.d.ts`);
+
+        CONSOLE_LOG(`adding "${fileName}" lib file`);
+
         if (!fs.existsSync(fileName)) throw new Error(`Lib file not found: ${fileName}`);
         if (visited.has(fileName)) return;
         visited.add(fileName);
